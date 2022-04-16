755c5078f24cb2c2de4f8c6c21c6c4d4f1a25df8
dbscks <dbscks2140@gmail.com>
2022-04-16 18:41:23 +0900

    NSchat First Screen 구현

---
+++ NSchat/app/src/main/java/com/example/nschat/How_many_cigarette.java
@@ -0,0 +1,38 @@
+package com.example.nschat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class How_many_cigarette extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_many_cigarette);
+
+
+        editText = findViewById(R.id.cigar);
+        editText.getText().toString();
+
+        Button imageButton = (Button) findViewById(R.id.cigar_count);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+}
