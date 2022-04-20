a861ef6128e98dc53e8d5588b019c36ba46b024d
Jogyuin <94088237+Jogyuin@users.noreply.github.com>
2022-04-20 17:02:11 +0900

    NSchat app chatting space
    NSchat app chatting xml and Activity
    
    but before interlocking with Dialogflow

---
+++ NSchat/app/src/main/java/com/example/nschat/BotReply.java
@@ -0,0 +1,8 @@
+package com.example.nschat;
+
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+
+public interface BotReply {
+
+    void callback(DetectIntentResponse returnResponse);
+}
---
+++ NSchat/app/src/main/java/com/example/nschat/ChattingActivity.java
@@ -0,0 +1,101 @@
+package com.example.nschat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class ChattingActivity extends AppCompatActivity implements BotReply {
+
+    RecyclerView chatView;
+    ChatAdapter chatAdapter;
+    List<Message> messageList = new ArrayList<>();
+    EditText editMessage;
+    ImageButton btnSend;
+
+    //dialogFlow
+    private SessionsClient sessionsClient;
+    private SessionName sessionName;
+    private String uuid = UUID.randomUUID().toString();
+    private String TAG = "mainactivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chatView = findViewById(R.id.chatView);
+        editMessage = findViewById(R.id.editMessage);
+        btnSend = findViewById(R.id.btnSend);
+
+        chatAdapter = new ChatAdapter(messageList, this);
+        chatView.setAdapter(chatAdapter);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                String message = editMessage.getText().toString();
+                if (!message.isEmpty()) {
+                    messageList.add(new Message(message, false));
+                    editMessage.setText("");
+                    sendMessageToBot(message);
+                    Objects.requireNonNull(chatView.getAdapter()).notifyDataSetChanged();
+                    Objects.requireNonNull(chatView.getLayoutManager())
+                            .scrollToPosition(messageList.size() - 1);
+                } else {
+                    Toast.makeText(ChattingActivity.this, "Please enter text!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        setUpBot();
+    }
+
+    private void setUpBot() {
+        try {
+            InputStream stream = this.getResources().openRawResource(R.raw.credential);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
+                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+            String projectId = ((ServiceAccountCredentials) credentials).getProjectId();
+
+            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
+            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(
+                    FixedCredentialsProvider.create(credentials)).build();
+            sessionsClient = SessionsClient.create(sessionsSettings);
+            sessionName = SessionName.of(projectId, uuid);
+
+            Log.d(TAG, "projectId : " + projectId);
+        } catch (Exception e) {
+            Log.d(TAG, "setUpBot: " + e.getMessage());
+        }
+    }
+
+    private void sendMessageToBot(String message) {
+        QueryInput input = QueryInput.newBuilder()
+                .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build();
+        new SendMessageInBg(this, sessionName, sessionsClient, input).execute();
+    }
+
+    @Override
+    public void callback(DetectIntentResponse returnResponse) {
+        if(returnResponse!=null) {
+            String botReply = returnResponse.getQueryResult().getFulfillmentText();
+            if(!botReply.isEmpty()){
+                messageList.add(new Message(botReply, true));
+                chatAdapter.notifyDataSetChanged();
+                Objects.requireNonNull(chatView.getLayoutManager()).scrollToPosition(messageList.size() - 1);
+            }else {
+                Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "failed to connect!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
