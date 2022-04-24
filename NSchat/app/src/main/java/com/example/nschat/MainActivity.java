6de91ae365e72a2c20025b3e00617c7bd5778a7e
dbscks <dbscks2140@gmail.com>
2022-04-25 01:16:18 +0900

    지도기능 추가

--- NSchat/app/src/main/java/com/example/nschat/MainActivity.java
+++ NSchat/app/src/main/java/com/example/nschat/MainActivity.java
@@ -7,12 +7,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
     public SharedPreferences prefs;
+    private Button ParkBtn;
+    private Button ChatBtn;
 
 
     @Override
@@ -38,5 +42,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button ChatBtn = findViewById(R.id.bot);
+        Button ParkBtn = findViewById(R.id.park);
+
+
+
+
+        ParkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        ChatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChattingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
 }
