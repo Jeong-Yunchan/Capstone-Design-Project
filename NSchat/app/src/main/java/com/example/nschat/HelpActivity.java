72435047e940dc8204b77fe9c74d0eb4c62e98fb
Jogyuin <94088237+Jogyuin@users.noreply.github.com>
2022-06-07 17:38:02 +0900

    delete calendar data and fix error
    delete calendar data and fix error

--- NSchat/app/src/main/java/com/example/nschat/HelpActivity.java
+++ NSchat/app/src/main/java/com/example/nschat/HelpActivity.java
@@ -3,25 +3,26 @@ package com.example.nschat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        ImageButton helpButton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        Button imageButton = (Button) findViewById(R.id.guideButton);
+        helpButton = findViewById(R.id.guideButton);
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        helpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), How_many_cigarette.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
